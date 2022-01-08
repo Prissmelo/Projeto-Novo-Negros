81a99c90e74e1a208bf512de116a5e76db6f9707
Priscila Melo <87330679+Prissmelo@users.noreply.github.com>
2022-01-08 00:55:05 -0300

    Add files via upload

---
+++ Controllers/NoticiasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NEGRASM.Controllers
+{
+    public class NoticiasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
